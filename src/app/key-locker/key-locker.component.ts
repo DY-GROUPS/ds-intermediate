34601618416afdc4335d46bbe0727f30eb1697d0
dmync <yashodhaariyarathne587@gmail.com>
2023-01-16 15:25:04 +0530

    modified keylocker-component

--- src/app/key-locker/key-locker.component.ts
+++ src/app/key-locker/key-locker.component.ts
@@ -324,19 +324,13 @@ export class KeyLockerComponent implements OnInit {
 
       },
       contractsWon: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['Single Auction House', 'Duplex Auction House', 'Commercial Auction House'],
         datasets: [
           {
             label: 'My First dataset',
-            backgroundColor: '#42A5F5',
-            borderColor: '#1E88E5',
-            data: [65, 59, 80, 81, 56, 55, 40]
-          },
-          {
-            label: 'My Second dataset',
-            backgroundColor: '#9CCC65',
-            borderColor: '#7CB342',
-            data: [28, 48, 40, 19, 86, 27, 90]
+            backgroundColor: '#ED8646',
+            borderColor: '#ED8646',
+            data: [250, 450, 300]
           }
         ]
 
@@ -381,9 +375,15 @@ export class KeyLockerComponent implements OnInit {
         legend: {display: false},
         maintainAspectRatio: false,
         scales: {
+          xAxes: [{
+            maxBarThickness: 40
+          }],
           yAxes: [{
             ticks: {
-               
+              
+              beginAtZero: true,
+              max: 500,
+              min: 0,
               fontFamily: 'Roboto', 
               fontSize: 12, 
               callback: (value)=> {
