de093182ec328e34bd20d1e1b199eb2474e7546d
dmync <yashodhaariyarathne587@gmail.com>
2023-02-16 13:07:31 +0530

    changed css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  
+  @ViewChild('pwd') pwd: ElementRef;
+  @ViewChild('icon') icon: ElementRef;
+
+  
+  
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+  showPassword(){
+
+    
+    const password = this.pwd.nativeElement;
+    const eyeIcon = this.icon.nativeElement;
+
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+
+    eyeIcon.classList.toggle("pi-eye");
+          
   }
 
 }
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,14 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
+  @ViewChild('pwd') pwd: ElementRef;
+  @ViewChild('icon') icon: ElementRef;
+
+  
+  
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   
-  myFunction(){
+  showPassword(){
 
+    
+    const password = this.pwd.nativeElement;
+    const eyeIcon = this.icon.nativeElement;
+
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+
+    eyeIcon.classList.toggle("pi-eye");
+          
   }
 
 }
