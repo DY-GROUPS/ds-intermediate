a71ee8db9c01c185567e3115f7a2ef9442b10f61
dmync <yashodhaariyarathne587@gmail.com>
2023-07-17 12:35:01 +0530

    send image

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -14,6 +14,9 @@ export class MessageComponent implements OnInit {
   text2;
   username = 'admin';
   messages: IMessage[] = [];
+  sendFileDetails = [];
+  uplodedFileContents =[];
+  image: string | SafeUrl ;
 
   @ViewChild('one', { static: false }) div1: ElementRef;
 
@@ -77,10 +80,25 @@ export class MessageComponent implements OnInit {
           const div2: HTMLElement = this.renderer.createElement('div');
           div2.className = "in-msg";
           div2.style.flexDirection = "column";
-          this.text2 = msgArray[index].msg;
-          div2.innerText = this.text2
-          mgsContainer.appendChild(div2);
-          this.div1.nativeElement.appendChild(mgsContainer);
+
+          if(msgArray[index].msg.dataType){
+
+            div2.style.flexDirection = "column";
+            const img: HTMLImageElement = this.renderer.createElement('img');
+            img.src = msgArray[index].msg.data;
+            div2.appendChild(img);
+            mgsContainer.appendChild(div2);
+            this.div1.nativeElement.appendChild(mgsContainer);
+  
+          }
+          else{
+  
+            this.text2 = msgArray[index].msg;
+            div2.innerText = this.text2
+            mgsContainer.appendChild(div2);
+            this.div1.nativeElement.appendChild(mgsContainer);
+  
+          }
   
         }
   
@@ -92,16 +110,133 @@ export class MessageComponent implements OnInit {
           const div2: HTMLElement = this.renderer.createElement('div');
           div2.className = "out-msg";
           div2.style.flexDirection = "column";
-          this.text2 = msgArray[index].msg;
-          div2.innerText = this.text2
-          mgsContainer.appendChild(div2);
-          this.div1.nativeElement.appendChild(mgsContainer);
+
+          if(msgArray[index].msg.dataType){
+
+            const img: HTMLImageElement = this.renderer.createElement('img');
+            img.src = msgArray[index].msg.data;
+            div2.appendChild(img);
+            mgsContainer.appendChild(div2);
+            this.div1.nativeElement.appendChild(mgsContainer);
+  
+          }
+          
+          else{
+  
+            this.text2 = msgArray[index].msg;
+            div2.innerText = this.text2
+            mgsContainer.appendChild(div2);
+            this.div1.nativeElement.appendChild(mgsContainer);
+  
+          }
   
         }
         
       }
 
 
+    });
+
+    this.chatservice.socket.on('send-img', (data) => {
+     
+      var msg = data.msg;
+      var datetime = data.datetime;
+
+      var index = this.messages.findIndex(el => el.username === this.username);
+
+      if(index === -1){
+
+        this.messages.push(
+
+          {
+            username: this.username,
+            msgs: [{type:'in',msg: {dataType: "img", data: msg}, datetime}],
+            isRead: false,
+            msgCount: 1,
+            userImage: './assets/notification-image4.png'
+          }
+
+        )
+
+      }
+
+      else{
+
+       this.messages[index].msgs.push({type:'in',msg: {dataType: "img", data: msg}, datetime});
+       this.messages[index].isRead = false;
+       this.messages[index].msgCount++
+
+      }
+
+      this.div1.nativeElement.innerHTML = null;
+  
+      var msgArray = this.messages[0].msgs
+      
+      for(var index = 0; index < msgArray.length; index++){
+  
+        if( msgArray[index].type === "in"){
+  
+          const mgsContainer: HTMLElement = this.renderer.createElement('div');
+          mgsContainer.className = "msg-container";
+          mgsContainer.style.justifyContent = "flex-start";
+          const div2: HTMLElement = this.renderer.createElement('div');
+          div2.className = "in-msg";
+          div2.style.flexDirection = "column";
+
+          if(msgArray[index].msg.dataType){
+
+            div2.style.flexDirection = "column";
+            const img: HTMLImageElement = this.renderer.createElement('img');
+            img.src = msgArray[index].msg.data;
+            div2.appendChild(img);
+            mgsContainer.appendChild(div2);
+            this.div1.nativeElement.appendChild(mgsContainer);
+  
+          }
+          else{
+  
+            this.text2 = msgArray[index].msg;
+            div2.innerText = this.text2
+            mgsContainer.appendChild(div2);
+            this.div1.nativeElement.appendChild(mgsContainer);
+  
+          }
+  
+        }
+  
+        else{
+  
+          const mgsContainer: HTMLElement = this.renderer.createElement('div');
+          mgsContainer.className = "msg-container";
+          mgsContainer.style.justifyContent = "flex-end";
+          const div2: HTMLElement = this.renderer.createElement('div');
+          div2.className = "out-msg";
+          div2.style.flexDirection = "column";
+
+          if(msgArray[index].msg.dataType){
+
+            const img: HTMLImageElement = this.renderer.createElement('img');
+            img.src = msgArray[index].msg.data;
+            div2.appendChild(img);
+            mgsContainer.appendChild(div2);
+            this.div1.nativeElement.appendChild(mgsContainer);
+  
+          }
+          
+          else{
+  
+            this.text2 = msgArray[index].msg;
+            div2.innerText = this.text2
+            mgsContainer.appendChild(div2);
+            this.div1.nativeElement.appendChild(mgsContainer);
+  
+          }
+  
+        }
+        
+      }
+
+      
     });
 
   } 
@@ -157,6 +292,163 @@ export class MessageComponent implements OnInit {
       this.text = '';
 
     }
+
+    if(this.sendFileDetails.length){
+
+      var index = this.messages.findIndex(el => el.username === this.username);
+      let d = new Date();
+      let datetime = d.toLocaleString();
+
+
+      for( let i= 0; i < this.sendFileDetails.length; i++){
+
+        div2.style.flexDirection = "column";
+        const img: HTMLImageElement = this.renderer.createElement('img');
+
+        if(this.sendFileDetails[i].type.includes("image")){
+
+          img.src = this.sendFileDetails[i].content;
+          img.addEventListener("click", this.viewImage.bind(img)); //Image zooming
+          div2.appendChild(img);
+          mgsContainer.appendChild(div2);
+          this.div1.nativeElement.appendChild(mgsContainer);
+          this.chatservice.socket.emit('send-img',{username: this.username, msg: this.sendFileDetails[i].content, src: this.sendFileDetails[i].content, name:this.sendFileDetails[i].name});
+      
+          
+        }
+
+        
+        //Update messages
+        if(index === -1){
+  
+          this.messages.push(
+    
+            {
+              username: this.username,
+              msgs: [{type:'out',msg: {dataType: "img", data: this.sendFileDetails[i].content}, datetime}],
+              isRead: true,
+              msgCount: 0,
+              userImage: './assets/notification-image4.png'
+            }
+    
+          )
+    
+        }
+    
+        else{
+    
+         this.messages[index].msgs.push({type:'out',msg: {dataType: "img", data: this.sendFileDetails[i].content}, datetime});
+         this.messages[index].isRead = true;
+         this.messages[index].msgCount = 0
+  
+        }
+
+       
+      }
+
+      this.sendFileDetails = [];
+      this.uplodedFileContents = [];
+     
+    }
+  }
+
+  async uploadFile(ev) {
+
+    debugger
+
+    const file = ev.target.files[0];
+
+    var fileType = file.type;
+    var fileName = file.name;
+
+    // console.log(fileType)
+  
+    
+    if(fileType.includes("image")){
+
+      this.image = this.sanitizer.bypassSecurityTrustUrl(
+        window.URL.createObjectURL(file)
+      );
+
+      this.uplodedFileContents.push(
+        {src:this.image,
+        name:fileName.substring(0, 6)});
+
+      const fileObjImg = await this.convertBase64(file);
+
+      var fileObjImgWithName = Object.assign({},fileObjImg,{name:fileName});
+
+      this.sendFileDetails.push(fileObjImgWithName);
+        
+    }
+
+    else{
+
+      this.image = './assets/message/fileicon.png';
+
+      this.uplodedFileContents.push(
+        {src:this.image,
+        name:fileName.substring(0, 6)});
+
+      const fileObjTxt = await this.convertBase64(file);
+
+      var fileObjTxtWithName = Object.assign({},fileObjTxt,{name:fileName});
+
+      this.sendFileDetails.push(fileObjTxtWithName);
+      
+
+    }
+
+  }
+
+
+  convertBase64 = (file) => {
+
+    return new Promise((resolve, reject) => {
+
+        const fileReader = new FileReader();
+
+        fileReader.readAsDataURL(file);
+
+        fileReader.onload = () => {
+
+            resolve({
+              type: fileReader.result.toString().split(';')[0],
+              content:fileReader.result.toString()
+            });
+        };
+
+        fileReader.onerror = (error) => {
+            reject(error);
+        };
+    });
+  };
+
+
+  removeImage(idx){
+
+    this.uplodedFileContents.splice(idx, 1);
+    this.sendFileDetails.splice(idx, 1);
+
+  }
+
+
+  viewImage(event) {
+
+    let viewer = new Viewer(event.currentTarget, {
+      navbar: false,
+      toolbar: false,
+      viewed() {
+
+        viewer.zoomTo(1);
+        viewer['image'].style.border = '10px solid white';
+        viewer['image'].style['border-radius'] = '10px';
+
+      }
+     
+    });
+
+    viewer.show();
   }
 
 }
