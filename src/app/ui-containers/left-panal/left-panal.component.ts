5d2beece200d8bfe16d55ba6f4d89da316dd0a6d
dmync <yashodhaariyarathne587@gmail.com>
2023-07-19 17:30:54 +0530

    modified

--- src/app/ui-containers/left-panal/left-panal.component.ts
+++ src/app/ui-containers/left-panal/left-panal.component.ts
@@ -113,7 +113,7 @@ export class LeftPanalComponent implements OnInit {
           case this._MainViews.settings:
             this.dashboardBtn = './assets/btn-icons/user.png';
             this.enterBtn = './assets/btn-icons/icons8_enter 1-gray.png';
-            this.lockerBtn = '/assets/btn-icons/icons8_key 1.png';
+            this.lockerBtn = './assets/btn-icons/icons8_key 1.png';
             this.settingsBtn = './assets/btn-icons/settings-white.png';
           break;
 
